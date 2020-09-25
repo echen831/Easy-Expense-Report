ee36906f0ddd48212ec5862827056009c8b3d2cc
your name here <echen831@gmail.com>
2020-09-25 16:18:11 -0700

    add basic structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { Header } from './Components/header' 
+import { Header } from './Components/header';
+import { Report } from './Components/report';
 import './styles.css';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Report/>
     </div>
   );
 }
---
+++ src/Components/report.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const Report = () => {
+    return (
+        <div className="report-container">
+            <ul>
+                <div>
+                    <span>Date</span>
+                    <span>Description</span>
+                    <span>Category</span>
+                    <span>Amount</span>
+                </div>
+
+            </ul>
+        </div>
+    )
+};
