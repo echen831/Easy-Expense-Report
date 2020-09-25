e217c9ca08f52dd512c1ff0ce03fd92342813dbf
your name here <echen831@gmail.com>
2020-09-25 16:00:42 -0700

    first commit

---
+++ src/Components/header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Header = () => {
+    return (
+        <div className='header-container'>
+            <h1>My Expense Report</h1>
+        </div>
+    )
+};
