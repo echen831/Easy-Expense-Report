17dbb0ae62bc239da24b3d2f0b6554afd484a1ef
your name here <echen831@gmail.com>
2020-09-26 15:35:45 -0700

    Updates

---
+++ precache-manifest.5a37100392885344350b5c30db3f61df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cde7792a4593f6c984dee34dbbbc1f17",
+    "url": "/EasyExpenseReport/index.html"
+  },
+  {
+    "revision": "84742fb85360a7d0bf83",
+    "url": "/EasyExpenseReport/static/css/main.26a01813.chunk.css"
+  },
+  {
+    "revision": "aae32a44fd08f5121318",
+    "url": "/EasyExpenseReport/static/js/2.941b7d64.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/EasyExpenseReport/static/js/2.941b7d64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84742fb85360a7d0bf83",
+    "url": "/EasyExpenseReport/static/js/main.d9ea57ad.chunk.js"
+  },
+  {
+    "revision": "8f1a98effc32449a6ee8",
+    "url": "/EasyExpenseReport/static/js/runtime-main.bc8ea80f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Easy-Expense-Report/precache-manifest.e764cc03de832372f1ada567a3076a27.js"
+  "/EasyExpenseReport/precache-manifest.5a37100392885344350b5c30db3f61df.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Easy-Expense-Report/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/EasyExpenseReport/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
