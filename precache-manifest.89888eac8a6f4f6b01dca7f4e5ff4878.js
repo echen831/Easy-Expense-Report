6b4fe83ed1987a4507aabc6d74a81d441fdc341e
your name here <echen831@gmail.com>
2020-09-26 14:44:59 -0700

    Updates

---
+++ precache-manifest.89888eac8a6f4f6b01dca7f4e5ff4878.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d12d97b64965c848c443ebd46ef9b9af",
+    "url": "/Easy-Expense-Report/index.html"
+  },
+  {
+    "revision": "8c647ca8d3254479e88a",
+    "url": "/Easy-Expense-Report/static/css/main.26a01813.chunk.css"
+  },
+  {
+    "revision": "417af530970b54416751",
+    "url": "/Easy-Expense-Report/static/js/2.e084a474.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Easy-Expense-Report/static/js/2.e084a474.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c647ca8d3254479e88a",
+    "url": "/Easy-Expense-Report/static/js/main.0f1e7e81.chunk.js"
+  },
+  {
+    "revision": "161c526623d909e8065b",
+    "url": "/Easy-Expense-Report/static/js/runtime-main.7934d80b.js"
+  }
+]);
