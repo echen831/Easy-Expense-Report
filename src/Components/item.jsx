c83b6f0300bead9fded3eb308ffebb232ed4e584
your name here <echen831@gmail.com>
2020-09-25 18:32:58 -0700

    add basic structure

--- src/Components/item.jsx
+++ src/Components/item.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useImperativeHandle, useState } from 'react';
 
 export const Item = (props) => {
+
+    const [desc, setDesc] = useState(props.desc)
+
+    const handleDescChange = (e) => {
+        setDesc(e.currentTarget.value);
+    }
 
     return (
         <div className='item-container'>
             <div>{props.date}</div>
-            <div>{props.desc}</div>
+            <div>
+                <form action="">
+                    <input type="text" value={desc} onChange={handleDescChange}/>
+                </form>
+            </div>
             <div>{props.cat}</div>
             <div>{props.ammt}</div>
         </div>
--- src/Components/report.jsx
+++ src/Components/report.jsx
@@ -4,7 +4,7 @@ import { Item } from './item';
 const DATA = [
     {date: '10/1/2019', desc: 'Dog chewing toy', cat: 'Pets', ammt: 100.00 },
     { date: '8/31/2020', desc: 'Cake for birthday', cat: 'Food', ammt: 50.00 },
-    { date: '9/20/2020', desc: 'Costo Run', cat: 'Groceries', ammt: 200.00 }
+    { date: '9/20/2020', desc: 'Costco Run', cat: 'Groceries', ammt: 200.00 }
 ]
 
 export const Report = () => {
