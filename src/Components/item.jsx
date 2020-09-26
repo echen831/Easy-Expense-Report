f184135cfc41dc6f625dfe1b8bc6a282da6130a2
your name here <echen831@gmail.com>
2020-09-25 18:41:05 -0700

    add basic structure

--- src/Components/item.jsx
+++ src/Components/item.jsx
@@ -1,16 +1,24 @@
 import React, { useImperativeHandle, useState } from 'react';
 
 export const Item = (props) => {
-
+    const [date, setDate] = useState(props.date)
     const [desc, setDesc] = useState(props.desc)
 
     const handleDescChange = (e) => {
         setDesc(e.currentTarget.value);
     }
 
+    const handleDateChange = (e) => {
+        setDate(e.currentTarget.value);
+    }
+
     return (
         <div className='item-container'>
-            <div>{props.date}</div>
+            <div>
+                <form action="">
+                    <input type="date" placeholder={date} onChange={handleDateChange}/>
+                </form>
+            </div>
             <div>
                 <form action="">
                     <input type="text" value={desc} onChange={handleDescChange}/>
